a4d0c9cf5fdbf641b801a0a7915fd8f699525152
Keashan Jayaweera <keashanjayaweera@gmail.com>
2020-09-27 10:56:18 +0530

    grid layout

--- Main.py
+++ Main.py
@@ -3,12 +3,13 @@
 # %%
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.slider import Slider
+#from kivy.uix.label import Label
+#from kivy.uix.button import Button
+#from kivy.uix.slider import Slider
 
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
+#from kivy.uix.gridlayout import GridLayout
+#from kivy.uix.textinput import TextInput
+from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 
 #from kivy.core.window import Window
@@ -16,7 +17,12 @@ from kivy.uix.widget import Widget
 #Window.clearcolor = (1, 1, 1, 1)
 
 class MyGrid(Widget):
-    pass
+    slider=ObjectProperty(None)
+    qc=ObjectProperty(None) 
+    sw=ObjectProperty(None)
+    
+    def quickchange(self):
+        print('Slider Value:',str(int(self.slider.value)))
 
 class MyApp(App):
     def build(self):
