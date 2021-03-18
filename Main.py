495e0ab0f8acd78d4c2773ddd9b57d8949deac73
Keashan Jayaweera <keashanjayaweera@gmail.com>
2021-03-18 21:38:32 +0530

    completed

--- Main.py
+++ Main.py
@@ -3,30 +3,30 @@
 # %%
 import kivy
 from kivy.app import App
-#from kivy.uix.label import Label
-#from kivy.uix.button import Button
-#from kivy.uix.slider import Slider
+from kivy.uix.slider import Slider
 
-#from kivy.uix.gridlayout import GridLayout
-#from kivy.uix.textinput import TextInput
 from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 
-#from kivy.core.window import Window
+from kivy.core.window import Window
+from plyer import notification
 
-#Window.clearcolor = (1, 1, 1, 1)
+
+Window.clearcolor = (146/255, 233/255, 253/255, 1)
 
 class MyGrid(Widget):
-    slider=ObjectProperty(None)
-    qc=ObjectProperty(None) 
-    sw=ObjectProperty(None)
+    slider = Slider(value_track=True, value_track_color=[1, 0, 0, 1])
+    #slider=ObjectProperty(None)
+    #qc=ObjectProperty(None) 
+    #sw=ObjectProperty(None)
     
     def quickchange(self):
+        notification.notify(title='Button Clicked',message='Slider Value:',app_icon=None,timeout=100)
         print('Slider Value:',str(int(self.slider.value)))
 
-class MyApp(App):
+class BlackScreenApp(App):
     def build(self):
         return MyGrid()
     
 if __name__=='__main__':
-    MyApp().run()
+    BlackScreenApp().run()
