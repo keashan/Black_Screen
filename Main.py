bbbf05bad19a01e1eab5fafbd6edcd56d70efa3f
Keashan Jayaweera <keashanjayaweera@gmail.com>
2021-06-10 01:10:21 +0530

    Add another screen

--- Main.py
+++ Main.py
@@ -4,29 +4,57 @@
 import kivy
 from kivy.app import App
 from kivy.uix.slider import Slider
+from kivy.lang import Builder
 
 from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 
 from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 from plyer import notification
 
 
 Window.clearcolor = (146/255, 233/255, 253/255, 1)
 
-class MyGrid(Widget):
+class FirstPage(Screen):
     slider = Slider(value_track=True, value_track_color=[1, 0, 0, 1])
     #slider=ObjectProperty(None)
     #qc=ObjectProperty(None) 
     #sw=ObjectProperty(None)
     
     def quickchange(self):
-        notification.notify(title='Button Clicked',message='Slider Value:',app_icon=None,timeout=100)
-        print('Slider Value:',str(int(self.slider.value)))
+        notification.notify(title='Button Clicked',message='Slider Value:' + str(self.slider.value/255),app_icon=None,timeout=100)
+        print('Slider Value:',str(self.slider.value))
+        blackscreen_app.screen_manager.current='BackPage'
+        #blackscreen_app.screen_manager.transition.direction="left"
+
+
+class BackPage(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv=Builder.load_file("BlackScreen.kv")
 
 class BlackScreenApp(App):
     def build(self):
-        return MyGrid()
+        self.screen_manager=ScreenManager()
+
+        self.first_page=FirstPage()
+        screen=Screen(name="FirstPage")
+        screen.add_widget(self.first_page)
+        self.screen_manager.add_widget(screen)
+
+        self.back_page=BackPage()
+        screen=Screen(name="BackPage")
+        screen.add_widget(self.back_page)
+        self.screen_manager.add_widget(screen)
+
+        blackscreen_app.screen_manager.current='FirstPage'
+        return kv
+
     
 if __name__=='__main__':
-    BlackScreenApp().run()
+    blackscreen_app=BlackScreenApp()
+    blackscreen_app.run()
